a84c1991bb7f140f517e51d4f400b13c88c0bca4
Akshay <akshayyadav7613@gmail.com>
2019-01-27 00:32:12 +0530

    1. number api working...
    2. now math related  fact working.....
    3. math,trivial,data all are working now..

--- app/src/main/java/com/example/akshay/numberapi/MainActivity.java
+++ app/src/main/java/com/example/akshay/numberapi/MainActivity.java
@@ -5,7 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,7 +22,11 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
     EditText editText;
     TextView textView;
-    String number ="http://numbersapi.com/";
+    String number="http://numbersapi.com/",add="math";
+    RadioGroup radioGroup;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +34,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         editText=findViewById(R.id.CityEditText);
         textView=findViewById(R.id.textView);
+        radioGroup=findViewById(R.id.radioGroup);
 
 
 
     }
 
+    public void pressed(View view) {
+        int radioid=radioGroup.getCheckedRadioButtonId();
+        switch (radioid)
+        { case R.id.radio0:
+            number ="http://numbersapi.com/";
+            add="math";
 
+            break;
+            case R.id.radio2:
+                number ="http://numbersapi.com/";
+                add ="";
+               break;
+
+            case R.id.radio3:
+                number ="http://numbersapi.com/";
+                add ="year";
+                break;
+
+        }
+    }
 
     public void getweather(View view)
 
@@ -46,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "enter the city name", Toast.LENGTH_SHORT).show();
                 textView.setText("");
             }else {
-                String weatherdetails = weatherinfo.execute(number+editText.getText().toString()).get();
+                String weatherdetails = weatherinfo.execute(number + editText.getText().toString()+add).get();
                 Log.i("get",weatherdetails);
 
                 textView.setText(weatherdetails);
@@ -57,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+
 
     public class Weatherinfo extends AsyncTask<String,Void,String>
     {
